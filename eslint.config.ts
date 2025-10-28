e2632adc3ccf4e4eb7a3c3aa2a52f8869f304924
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-10-28 13:09:25 -0300

    chore: Update ESLint configuration to include '@stylistic/eslint-plugin' with custom rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,20 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  { files: ["./src/**/*.ts"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["./src/**/*.ts"],
+    plugins: {
+      js,
+      "@stylistic": stylistic,},
+    rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "never"],
+    },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node } },
+
   tseslint.configs.recommended,
 ]);
