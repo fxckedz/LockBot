996a39ed8bac1eca644099c850dbb1863f9655f1
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-11-05 12:03:42 -0300

    fix: add environment variable defaults to Config class

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -2,4 +2,7 @@ import "dotenv/config"
 
 export class Config{
   static TOKEN = process.env.TOKEN || ""
+  static URL = process.env.URL || ""
+  static LOGIN = process.env.LOGIN || ""
+  static PASSWORD = process.env.PASSWORD || ""
 }
