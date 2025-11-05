413e2f2d8b73472d3a5d4a09bf5d4c6e8e7f41c0
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-11-05 15:15:07 -0300

    chore: add Wslock class

---
+++ src/service/WslockService.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @stylistic/quotes */
+import axios from "axios"
+import { Config } from "../config/Config.js"
+
+class WslockService{
+  constructor(
+    private readonly url:string,
+    private readonly login:string,
+    private readonly password:string
+  ){}
+
+  async getLocksList(){
+
+    const escapedPassword = this.password.replace(/&/g, '&amp;')
+
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Header>
+<Seguranca xmlns="http://www.pentasis.com.br/">
+<Usuario>${this.login}</Usuario>
+<Senha>${escapedPassword}</Senha>
+</Seguranca>
+</soap:Header>
+<soap:Body>
+<LocksList xmlns="http://www.pentasis.com.br/" />
+</soap:Body>
+</soap:Envelope>`
+    
+    const response = await axios.post(this.url, xml, {
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        "SOAPAction": "http://www.pentasis.com.br/LocksList"
+      },
+      timeout: 10000
+    })
+
+    return response.data
+  }
+}
+
+export default new WslockService(Config.URL, Config.LOGIN, Config.PASSWORD)
