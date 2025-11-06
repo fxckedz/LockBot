ae650b707d1fb93fdee2696d8bb90cd079560d8b
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-11-06 16:42:06 -0300

    feat: make messagehandler log the response

--- src/bot/handler/MessageHandler.ts
+++ src/bot/handler/MessageHandler.ts
@@ -1,7 +1,34 @@
 import type { Context } from "grammy"
+import wslockService from "../../service/WslockService.js"
 
 export class MessageHandler {
-  static handle(ctx: Context): void {
-    console.log(ctx.message?.text)
+  static async handle(ctx: Context): Promise<void> {
+    const command = ctx.message?.text
+    const input = command?.toLowerCase().split("#") || []
+
+    const action = input[0]
+    const matricula = ctx.chat?.first_name || ""
+    
+    switch (action) {
+      case "abrir": {
+        
+        const senhaAbertura = input[1] ? String(input[1]) : ""
+        const senhaOperador = input[2] ? String(input[2]) : ""
+
+        const response = await wslockService.ensurePassword(matricula, senhaAbertura, senhaOperador)
+
+        console.log(response)
+        break
+      }
+    
+      case "fechar": {
+        const senhaFechamento = input[1] || ""
+
+        const response = await wslockService.confirmClosing(matricula, senhaFechamento)
+
+        console.log(response)
+        break
+      }
+    }
   }
 }
