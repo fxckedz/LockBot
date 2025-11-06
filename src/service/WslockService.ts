575ee681c348a05c7b49b727e40e7cd02b598da4
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-11-06 11:02:09 -0300

    feat: add getLockListByUser

--- src/service/WslockService.ts
+++ src/service/WslockService.ts
@@ -43,6 +43,40 @@ class WslockService{
 
     return parseSoapResponse.handle(soapData)
   }
+
+  async getLockListByUser(matricula: string){
+
+    const escapedPassword = this.escapePassword(this.password)
+    
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Header>
+<Seguranca xmlns="http://www.pentasis.com.br/">
+<Usuario>${this.login}</Usuario>
+<Senha>${escapedPassword}</Senha>
+</Seguranca>
+</soap:Header>
+<soap:Body>
+<LocksListByUser xmlns="http://www.pentasis.com.br/">
+<matriculaop>${matricula}</matriculaop>
+</LocksListByUser>
+</soap:Body>
+</soap:Envelope>`
+
+    const response = await axios.post(this.url, xml, {
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        "SOAPAction": "http://www.pentasis.com.br/LocksListByUser"
+      },
+      timeout: 10000
+    })
+
+    const soapData = response.data
+
+    return parseSoapResponse.handle(soapData)
+
+  }
+
 }
 
 export default new WslockService(Config.URL, Config.LOGIN, Config.PASSWORD)
